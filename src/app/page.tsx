a621d6ad35e9ae72e4d2ceb22884d9d5749b8e2c
Ravi Shankar Jha <ravi.jha3369@gmail.com>
2024-12-21 16:22:03 +0530

    fix: updated code commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,12 @@
 import { IngredientForm } from "@/component/FormComponent/page";
-import GptChat from "@/component/serverComponent/page";
 
-export default function Home() {
+export default async function Home(){
+
   return (
     <div className="container mx-auto flex justify-center items-center min-h-screen">
       <div className="w-full max-w-lg">
         <h1 className="text-center">List of ingredients</h1>
-        <IngredientForm />
-        <GptChat />
+          <IngredientForm />
       </div>
     </div>
   );
--- src/component/FormComponent/page.tsx
+++ src/component/FormComponent/page.tsx
@@ -6,77 +6,81 @@ import { z } from "zod";
 
 import { Button } from "@/components/ui/button";
 import {
-	Form,
-	FormControl,
-	FormDescription,
-	FormField,
-	FormItem,
-	FormLabel,
-	FormMessage,
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import { useState } from "react";
 import GptChat from "../serverComponent/page";
 
 const formSchema = z.object({
-	ingredients: z.array(z.object({ ingredient: z.string() })),
+  ingredients: z.array(z.string()),
 });
 
 export function IngredientForm() {
-	const form = useForm<z.infer<typeof formSchema>>({
-		resolver: zodResolver(formSchema),
-		defaultValues: {
-			ingredients: [{ ingredient: "" }],
-		},
-	});
+  const [showResponse, setShowResponse] = useState(false);
+  const [message, setMessage] = useState("");
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      ingredients: [""],
+    },
+  });
 
-	function onSubmit(values: z.infer<typeof formSchema>) {
-        const queryParams = new URLSearchParams();
-		let ingredientsList = '';
-		for (const ingredient of values.ingredients) {
-			ingredientsList += `${ingredient},`;
-		}
-        queryParams.append('ingredients', ingredientsList);
-        const url = `?${queryParams.toString()}`;
-        window.location.href = url;
-	}
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    const queryParams = new URLSearchParams();
+    queryParams.append('ingredients', values.ingredients.join(','));
+    const url = `?${queryParams.toString()}`;
+    window.location.href = url;
+    setMessage(values.ingredients.join(','))
+    setShowResponse(true);
+  }
 
-	function addIngredient() {
-		form.setValue("ingredients", [
-			...form.getValues("ingredients"),
-			{ ingredient: "" },
-		]);
-	}
+  function addIngredient() {
+    form.setValue("ingredients", [
+      ...form.getValues("ingredients"),
+      "",
+    ]);
+  }
 
-	return (
-		<Form {...form}>
-			<form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-				{form.watch("ingredients").map((_, index) => (
-					<FormField
-						// biome-ignore lint/suspicious/noArrayIndexKey: <explanation>
-						key={index}
-						control={form.control}
-						name={`ingredients.${index}.ingredient`}
-						render={({ field }) => (
-							<FormItem>
-								<FormLabel>Ingredient {index + 1}</FormLabel>
-								<FormControl>
-									<Input placeholder={`Ingredient ${index + 1}`} {...field} />
-								</FormControl>
-								<FormDescription>
-									Enter the name of the ingredient.
-								</FormDescription>
-								<FormMessage />
-							</FormItem>
-						)}
-					/>
-				))}
-                <div className="flex justify-between w-full my-8">
-                    <Button type="button" onClick={addIngredient}>
-                        Add More Ingredient
-                    </Button>
-                    <Button type="submit">Submit</Button>
-                </div>
-			</form>
-		</Form>
-	);
+  return (
+    <>
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        {form.watch("ingredients").map((_, index) => (
+          <FormField
+            // biome-ignore lint/suspicious/noArrayIndexKey: <explanation>
+            key={index}
+            control={form.control}
+            name={`ingredients.${index}`}
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Ingredient {index + 1}</FormLabel>
+                <FormControl>
+                  <Input placeholder={`Ingredient ${index + 1}`} {...field} />
+                </FormControl>
+                <FormDescription>
+                  Enter the name of the ingredient.
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        ))}
+        <div className="flex justify-between w-full my-8">
+          <Button type="button" onClick={addIngredient}>
+            Add More Ingredient
+          </Button>
+          <Button type="submit">Submit</Button>
+        </div>
+      </form>
+    </Form>
+    {showResponse && message ?<GptChat ingredientList={message}/>:null}
+    </>
+  );
 }
